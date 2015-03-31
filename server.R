3186dc378824fc3f99a7e90cfdc22b76f70830ea
outcomeproject <jose.bartolomei@outcomeproject.com>
2015-03-31 16:38:44 -0400

    Add labs cases and change plot width

--- server.R
+++ server.R
@@ -1,61 +1,64 @@
 
- # Load data created in the above steps ----------------------------------------------
- # To minimize work load the data to be used was saved as an .rData
+# Load data created in the above steps ----------------------------------------------
+# To minimize work load the data to be used was saved as an .rData
 load(file = "./trend_data.RData")
 
- # Plot ------------------------------------------------------------------------------
- 
- library(ggplot2)
- shinyServer(function(input, output){
-   output$plot <- renderPlot({
-     
-    if(input$outcome == "All"){
-     trend.data <- trend.data
-    }
-     if(input$outcome != "All"){
-       trend.data <- trend.data[trend.data$Disease == c(input$outcome),]
-     }
+# Plot ------------------------------------------------------------------------------
 
-# Plot
-p <- 
-  ggplot(trend.data, aes(x = Year, y = Prevalence * 100 , colour = Disease,
-                  group = interaction(weight, Disease),
-                  label = round(Prevalence * 100, 2))) +
-  geom_line(stat = "identity", size = 1.5) +
-  labs(x = "Survey Year", y = "Prevalence * 100") +
-  geom_text(size = 5, hjust = 0, vjust = -1, colour = "black") +
-  labs(title = "Outcomes Prevalence Trend Among Adults in Puerto Rico, BRFSS 2008 - 2013") +
-  theme(plot.title = element_text(size = 25, face="bold")) +
-  theme(axis.text.x = element_text(colour = 'black', angle = 0, 
-                                   size = 25, hjust = 0.5, vjust = 0)) +
-  theme(axis.text.y = element_text(colour = 'black', angle = 0, 
-                                   size = 25, hjust = 0.5, vjust = 0)) +
-  scale_y_continuous(limits = c(0, 20)) +
-  geom_vline(xintercept = 4, colour="red", linetype = "longdash") +
-  theme(legend.text=element_text(size = 10)) +
-  geom_text(aes(x= 3.95, label = "Change in weighting Scheme", y = 6), 
-                colour="red", angle = 90, size = 5) +
-  geom_point(size = 4) +
-	theme(axis.title=element_text(size=25,face="bold"))
+library(ggplot2)
+shinyServer(function(input, output){
+	output$plot <- renderPlot({
+		
+		if(input$outcome == "All"){
+			trend.data <- trend.data
+		}
+		if(input$outcome != "All"){
+			trend.data <- trend.data[trend.data$Disease == c(input$outcome),]
+		}
+		
+		# Plot
+		p <- 
+			ggplot(trend.data, aes(x = Year, y = Prevalence * 100 , colour = Disease,
+														 group = interaction(weight, Disease),
+														 label = round(Prevalence * 100, 2))) +
+			geom_line(stat = "identity", size = 1.5) +
+			labs(x = "Survey Year", y = "Prevalence * 100") +
+			geom_text(size = 5, hjust = 0, vjust = -1, colour = "black") +
+			labs(title = "Outcomes Prevalence Trend Among Adults in Puerto Rico, BRFSS 2008 - 2013") +
+			theme(plot.title = element_text(size = 25, face="bold")) +
+			theme(axis.text.x = element_text(colour = 'black', angle = 0, 
+																			 size = 25, hjust = 0.5, vjust = 0)) +
+			theme(axis.text.y = element_text(colour = 'black', angle = 0, 
+																			 size = 25, hjust = 0.5, vjust = 0)) +
+			scale_y_continuous(limits = c(0, 20)) +
+			geom_vline(xintercept = 4, colour="red", linetype = "longdash") +
+			theme(legend.text=element_text(size = 10)) +
+			geom_text(aes(x= 3.95, label = "Change in weighting Scheme", y = 6), 
+								colour="red", angle = 90, size = 5) +
+			geom_point(size = 4) +
+			theme(axis.title=element_text(size=25,face="bold"))
+		
+		
+		## ConfInf == TRUE
+		errB <- geom_errorbar(aes(ymax = ul * 100, ymin = ll * 100), width = 0.2)
+		newlab <- labs(x = "Survey Year", y = "Number of Cases") 
+		
+		if(input$values == "None" & input$ConfInt == TRUE){
+			p + aes(label = "") + errB
+		} else if(input$values == "Prevalence" & input$ConfInt == TRUE){
+			p + aes(label = round(Prevalence * 100, 2)) + errB
+		} else if(input$values == "Cases" & input$ConfInt == TRUE){
+			p + aes(label = format(Cases, big.mark = ",")) + errB + newlab
+			##### ConfInf == FALSE
+		} else if(input$values == "None" & input$ConfInt == FALSE){
+			p + aes(label = "") 
+		} else if(input$values == "Prevalence" & input$ConfInt == FALSE){
+			p + aes(label = round(Prevalence * 100, 2))
+		} else if(input$values == "Cases" & input$ConfInt == FALSE){
+			p + aes(label = format(Cases, big.mark = ",")) + newlab
+		}
+		
+		
+	})
 	
-
-## ConfInf == TRUE
-errB <- geom_errorbar(aes(ymax = ul * 100, ymin = ll * 100), width = 0.2)
-if(input$values == "None" & input$ConfInt == TRUE){
-	p + aes(label = "") + errB
-} else if(input$values == "Prevalence" & input$ConfInt == TRUE){
-	p + aes(label = round(Prevalence * 100, 2)) + errB
-} else if(input$values == "Cases" & input$ConfInt == TRUE){
-	p + aes(label = format(Cases, big.mark = ",")) + errB
-	##### ConfInf == FALSE
-} else if(input$values == "None" & input$ConfInt == FALSE){
-	p + aes(label = "") 
-} else if(input$values == "Prevalence" & input$ConfInt == FALSE){
-	p + aes(label = round(Prevalence * 100, 2))
-} else if(input$values == "Cases" & input$ConfInt == FALSE){
-	p + aes(label = format(Cases, big.mark = ","))
-}
-
 })
- 
- })
--- ui.R
+++ ui.R
@@ -1,33 +1,33 @@
 #User interface BRFSS trend Shiny
 
 shinyUI(fluidPage(
- titlePanel("Puerto Rico Outcomes Trend"),
- sidebarLayout(
- 	
-  sidebarPanel(
-    checkboxInput("ConfInt", label = "95% Confidence Intervals", value = FALSE),
-    
-   selectInput("outcome", "Chose an Outcome",
-choices= c("All", "Asthma", "Diabetes", "Coronary Heart Disease",
-           "Infarction", "Stroke", "Tobacco Use")),
-
-selectInput("values", "Chose values to display",
-						choices= c("None", "Prevalence", "Cases")),
-
-
-br(),
-h4(span("Developed by", style = "color:grey")),
-img(src = "Outcome_Project_Logo.png", height = 72, width = 200),
-br(),
-br(),
-h6(span("Powered by RStudio™ Shiny", style = "color:grey")),
-
-width = 3),
-
-
-mainPanel(plotOutput("plot"),
-          width = 15)
- )
+	titlePanel("Puerto Rico Outcomes Trend"),
+	sidebarLayout(
+		
+		sidebarPanel(
+			checkboxInput("ConfInt", label = "95% Confidence Intervals", value = FALSE),
+			
+			selectInput("outcome", "Chose an Outcome",
+									choices= c("All", "Asthma", "Diabetes", "Coronary Heart Disease",
+														 "Infarction", "Stroke", "Tobacco Use")),
+			
+			selectInput("values", "Chose values to display",
+									choices= c("None", "Prevalence", "Cases")),
+			
+			
+			br(),
+			h4(span("Developed by", style = "color:grey")),
+			img(src = "Outcome_Project_Logo.png", height = 72, width = 200),
+			br(),
+			br(),
+			h6(span("Powered by RStudio™ Shiny", style = "color:grey")),
+			
+			width = 3),
+		
+		
+		mainPanel(plotOutput("plot"),
+							width = 8)
+	)
 ))
 
 
